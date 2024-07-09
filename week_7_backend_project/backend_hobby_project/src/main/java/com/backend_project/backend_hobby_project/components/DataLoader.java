04dda7c5aef1cc077150aae10eaca6cb8b0c866c
Robbie Mcilwaine <robbiemcilwaine99@gmail.com>
2024-07-09 13:17:54 +0100

    reseeded dataloader

--- week_7_backend_project/backend_hobby_project/src/main/java/com/backend_project/backend_hobby_project/components/DataLoader.java
+++ week_7_backend_project/backend_hobby_project/src/main/java/com/backend_project/backend_hobby_project/components/DataLoader.java
@@ -40,8 +40,19 @@ public class DataLoader implements ApplicationRunner {
         Hobby fiveAside = new Hobby("Five a Side");
         hobbyRepository.save(fiveAside);
 
+//        NEW HOBBIES
+        Hobby paintBalling = new Hobby("Paintballing");
+        hobbyRepository.save(paintBalling);
+        Hobby pottery = new Hobby("Pottery");
+        hobbyRepository.save(pottery);
+
+//        NEW VENUES
         Venue wembly = new Venue("Wembly", "London", 90000);
         venueService.addVenue(wembly);
+        Venue digbeth = new Venue("Digbeth", "Birmingham", 20);
+        venueService.addVenue(digbeth);
+        Venue balham = new Venue("Balham", "London", 20);
+        venueService.addVenue(balham);
 
         List<DaysOfTheWeek> sunnyAvailability = new ArrayList<>();
         sunnyAvailability.add(DaysOfTheWeek.FRIDAYMORNING);
@@ -55,6 +66,20 @@ public class DataLoader implements ApplicationRunner {
         mariaAvailability.add(DaysOfTheWeek.WEDNESDAYAFTERNOON);
         mariaAvailability.add(DaysOfTheWeek.TUESDAYEVENING);
 
+        List<DaysOfTheWeek> adilAvailability = new ArrayList<>();
+        adilAvailability.add(DaysOfTheWeek.TUESDAYEVENING);
+        adilAvailability.add(DaysOfTheWeek.SATURDAYAFTERNOON);
+
+        List<DaysOfTheWeek> denaAvailability = new ArrayList<>();
+        denaAvailability.add(DaysOfTheWeek.WEDNESDAYMORNING);
+        denaAvailability.add(DaysOfTheWeek.FRIDAYEVENING);
+
+//        NEW USERS
+        User adil = new User("Adil", 29, "London", "Lorem Ipsum", false, adilAvailability);
+        userService.addUser(adil);
+        User dena = new User("Dena", 23, "London", "Lorem Ipsum", false, denaAvailability);
+        userService.addUser(dena);
+
         User sunny = new User("Sunny", 26, "Birmingham", "Lorem Ipsum", false, sunnyAvailability);
         userService.addUser(sunny);
         User dan = new User("Dan", 22, "Coventry", "Lorem Ipsum", false, danAvailability);
@@ -66,11 +91,31 @@ public class DataLoader implements ApplicationRunner {
         userService.addHobbyToUser(fiveAside.getId(), dan.getId());
         userService.addHobbyToUser(fiveAside.getId(), maria.getId());
 
-        Booking booking = new Booking("18:00", "11/06/2024", wembly, fiveAside);
-        bookingRepository.save(booking);
-        bookingService.addUserToBooking(sunny.getId(), booking.getId());
-        bookingService.addUserToBooking(dan.getId(), booking.getId());
-        bookingService.addUserToBooking(maria.getId(), booking.getId());
-        bookingRepository.save(booking);
+//        ASSIGN NEW HOBBIES TO NEW USERS
+        userService.addHobbyToUser(paintBalling.getId(), adil.getId());
+        userService.addHobbyToUser(pottery.getId(), adil.getId());
+        userService.addHobbyToUser(paintBalling.getId(), dena.getId());
+        userService.addHobbyToUser(fiveAside.getId(), dena.getId());
+        userService.addHobbyToUser(pottery.getId(), dena.getId());
+
+
+
+        Booking booking1 = new Booking("18:00", "11/06/2024", wembly, fiveAside);
+        bookingRepository.save(booking1);
+        bookingService.addUserToBooking(sunny.getId(), booking1.getId());
+        bookingService.addUserToBooking(dan.getId(), booking1.getId());
+        bookingService.addUserToBooking(maria.getId(), booking1.getId());
+        bookingRepository.save(booking1);
+
+//        NEW BOOKINGS
+        Booking booking2 = new Booking("14:00", "13/06/2024", balham , paintBalling);
+        Booking booking3 = new Booking("10:00", "10/06/2024", digbeth, pottery);
+        bookingRepository.save(booking2);
+        bookingRepository.save(booking3);
+        bookingService.addUserToBooking(adil.getId(), booking2.getId());
+        bookingService.addUserToBooking(dena.getId(), booking3.getId());
+        bookingService.addUserToBooking(dena.getId(), booking2.getId());
+        bookingRepository.save(booking2);
+        bookingRepository.save(booking3);
        }
 }
